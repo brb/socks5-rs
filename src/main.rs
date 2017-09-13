14216474ad57e0c276029decaaba008f0ceefd1d
Martynas Pumputis <martynasp@gmail.com>
2017-09-13 09:32:55 +0200

    Works (reconnect doesn't though)

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,37 @@
+// TODO handle close connection
+// 1. Make it work
+// 2. Cleanup
+// 3. Multi threaded
 extern crate mio;
 extern crate bytes;
 
-use mio::*;
-use mio::tcp::{TcpListener, TcpStream};
 use std::collections::HashMap;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use std::io::{Read, Write};
-use bytes::{BytesMut, BufMut, ByteOrder, BigEndian};
 use std::io::ErrorKind;
 use std::net::IpAddr::V4;
-use std::rc::Rc;
+use bytes::{BytesMut, BufMut, ByteOrder, BigEndian};
+use mio::*;
+use mio::tcp::{TcpListener, TcpStream};
 
 #[derive(Debug)]
 struct Conn {
     socket: TcpStream, // client <-> proxy conn
-    token: Token,
     remote_socket: Option<TcpStream>, // proxy <-> server
-    remote_token: Token,
     state: State,
     read_buf: BytesMut,
-    write_buf: BytesMut,
-    next_event: Event,
-    client_proxy: bool,
+    remote_buf: BytesMut,
+    already_write: bool,
+    already_remote_write: bool,
 }
 
 #[derive(PartialEq, Clone, Copy, Debug)]
 enum Event {
     Read,
     Write,
+    RemoteRead,
     RemoteWrite,
-    Register,
-    Nil,
+    RemoteRegister,
 }
 
 #[derive(PartialEq, Debug)]
@@ -39,7 +40,6 @@ enum State {
     WriteFirstReply,
     ReadSecondRequest,
     WriteSecondReply,
-    WaitForTerminate,
     Proxy,
 }
 
@@ -65,85 +65,70 @@ fn main() {
             match event.token() {
                 SERVER => {
                     if event.readiness().is_readable() {
-                        next_token_index += 1;
+                        next_token_index += 2;
                         accept_connection(&server, next_token_index, &poll, &mut conns);
                     }
                 },
                 token => {
-                    let mut insert = false;
-                    let mut to_insert = None;
-                    {
-                        let mut rc_conn = conns.get_mut(&token).unwrap();
-                        {
-                            println!("ref count: {}", Rc::strong_count(rc_conn));
-                            let mut conn = Rc::get_mut(rc_conn).unwrap();
+                    let real_token = Token(token.0 + (token.0 % 2)); // get original token
+                    let mut conn = conns.get_mut(&real_token).unwrap();
 
-                        // TODO handle close connection
-                        match handle_event(conn, token, event.readiness()) {
-                            Ok(Event::Read) => {
-                                poll.reregister(
-                                    &conn.socket,
-                                    token,
-                                    Ready::readable(),
-                                    PollOpt::edge() | PollOpt::oneshot()
-                                ).unwrap();
-                            },
-                            Ok(Event::Write) => {
-                                poll.reregister(
-                                    &conn.socket,
-                                    token,
-                                    Ready::writable(),
-                                    PollOpt::edge() | PollOpt::oneshot()
-                                ).unwrap();
-                            },
-                            Ok(Event::RemoteWrite) => {
-                                if let Some(ref c) = conn.remote_socket {
-                                    poll.reregister(
-                                        c,
-                                        conn.remote_token,
-                                        Ready::writable(),
-                                        PollOpt::edge() | PollOpt::oneshot()
-                                    ).unwrap();
+                    match handle_event(conn, token, event.readiness()) {
+                        Err(e) => {
+                            panic!("handle_event: {}", e);
+                        },
+                        Ok(events) => {
+                            for ev in events {
+                                match ev {
+                                    Event::Read => {
+                                        poll.reregister(
+                                            &conn.socket,
+                                            real_token,
+                                            Ready::readable(),
+                                            PollOpt::edge() | PollOpt::oneshot()
+                                        ).unwrap();
+                                    },
+                                    Event::Write => {
+                                        poll.reregister(
+                                            &conn.socket,
+                                            real_token,
+                                            Ready::writable(),
+                                            PollOpt::edge() | PollOpt::oneshot()
+                                        ).unwrap();
+                                    },
+                                    Event::RemoteRead => {
+                                        if let Some(ref socket) = conn.remote_socket {
+                                            poll.reregister(
+                                                socket,
+                                                Token(real_token.0 - 1),
+                                                Ready::readable(),
+                                                PollOpt::edge() | PollOpt::oneshot()
+                                            ).unwrap();
+                                        }
+                                    },
+                                    Event::RemoteWrite => {
+                                        if let Some(ref socket) = conn.remote_socket {
+                                            poll.reregister(
+                                                socket,
+                                                Token(real_token.0 - 1),
+                                                Ready::writable(),
+                                                PollOpt::edge() | PollOpt::oneshot()
+                                            ).unwrap();
+                                        }
+                                    },
+                                    Event::RemoteRegister => {
+                                        if let Some(ref socket) = conn.remote_socket {
+                                            poll.register(
+                                                socket,
+                                                Token(real_token.0 - 1),
+                                                Ready::readable(),
+                                                PollOpt::edge() | PollOpt::oneshot()
+                                            ).unwrap();
+                                        }
+                                    },
                                 }
-                            },
-                            Ok(Event::Register) => {
-                                poll.reregister(
-                                    &conn.socket,
-                                    token,
-                                    Ready::writable(),
-                                    PollOpt::edge() | PollOpt::oneshot()
-                                ).unwrap();
-                                insert = true;
-                            },
-                            Ok(Event::Nil) => {
-                                panic!("event::nil");
-                            },
-                            Err(_) => {
-                                panic!("err");
-                            },
-                        } // END match
-                        }
-                        if insert {
-                            {
-                                let mut foo = Rc::get_mut(rc_conn).unwrap();
-                                next_token_index += 1;
-                                foo.remote_token = Token(next_token_index);
                             }
-                            to_insert = Some(rc_conn.clone());
-                        }
-                    }
-                    if insert {
-                        let mut omg = to_insert.unwrap();
-                        if let Some(ref connz) = omg.remote_socket {
-                            conns.insert(omg.remote_token, omg.clone());
-                            poll.register(
-                                        connz,
-                                        omg.remote_token,
-                                        Ready::readable(),
-                                        PollOpt::edge() | PollOpt::oneshot()
-                                    ).unwrap();
-
-                        }
+                        },
                     }
                 }
             }
@@ -151,85 +136,72 @@ fn main() {
     }
 }
 
-fn handle_event(conn: &mut Conn, token: Token, readiness: Ready) -> Result<Event, &'static str> {
-        // sanity checks
-        if !(readiness.is_writable() && conn.next_event == Event::Write) &&
-            !(readiness.is_readable() && conn.next_event == Event::Read) {
-                return Err("wtf");
-        }
-
-        match conn.state {
-            State::ReadFirstRequest =>
-                handle_read_first_request(conn),
-            State::WriteFirstReply =>
-                handle_write_first_reply(conn),
-            State::ReadSecondRequest =>
-                handle_read_second_request(conn),
-            State::WriteSecondReply =>
-                handle_write_second_reply(conn),
+fn handle_event(conn: &mut Conn, token: Token, readiness: Ready) -> Result<Vec<Event>, &'static str> {
+        let events = match conn.state {
+            State::ReadFirstRequest => handle_read_first_request(conn),
+            State::WriteFirstReply => handle_write_first_reply(conn),
+            State::ReadSecondRequest => handle_read_second_request(conn),
+            State::WriteSecondReply => handle_write_second_reply(conn),
             State::Proxy => {
-                let is_client_conn: bool = token == conn.token;
-                handle_proxy(conn, is_client_conn, readiness.is_readable());
+                let is_client_conn = token.0 % 2 == 0;
+                handle_proxy(conn, is_client_conn, readiness.is_readable())
             },
-            State::WaitForTerminate =>
-                panic!("NYI"),
-        }
+        };
 
-        return Ok(conn.next_event);
+        return Ok(events);
     }
 
-fn accept_connection(server: &TcpListener, token_index: usize, poll: &Poll, conns: &mut HashMap<Token, Rc<Conn>>) {
+fn accept_connection(server: &TcpListener, token_index: usize, poll: &Poll, conns: &mut HashMap<Token, Conn>) {
     let socket = server.accept().unwrap().0;
     let token = Token(token_index);
     poll.register(&socket, token, Ready::readable(), PollOpt::edge() | PollOpt::oneshot()).unwrap();
 
     let conn = Conn{
         socket: socket,
-        token: token,
         remote_socket: None,
-        remote_token: Token(0),
         state: State::ReadFirstRequest,
         read_buf: BytesMut::with_capacity(64 * 1024),
-        write_buf: BytesMut::with_capacity(64 * 1024),
-        next_event: Event::Read,
-        client_proxy: true,
+        remote_buf: BytesMut::with_capacity(64 * 1024),
+        already_write: false,
+        already_remote_write: false,
     };
-    conns.insert(token, Rc::new(conn));
+    conns.insert(token, conn);
 }
 
 // TODO trait for states
 
-fn handle_read_first_request(conn: &mut Conn) {
+fn handle_read_first_request(conn: &mut Conn) -> Vec<Event> {
     let socket = &mut conn.socket;
     read_until_would_block(socket, &mut conn.read_buf).unwrap();
 
     let len = conn.read_buf.len();
     if len < 2 || len < conn.read_buf[1] as usize + 2 {
-        return;
+        return vec![Event::Read];
     }
 
     conn.state = State::WriteFirstReply;
-    conn.next_event = Event::Write;
     conn.read_buf.clear();
+
+    return vec![Event::Write];
 }
 
-fn handle_write_first_reply(conn: &mut Conn) {
+fn handle_write_first_reply(conn: &mut Conn) -> Vec<Event> {
     let socket = &mut conn.socket;
     let reply = [5, 0];
     let size = socket.write(&reply).unwrap();
     assert_eq!(size, reply.len());
 
     conn.state = State::ReadSecondRequest;
-    conn.next_event = Event::Read;
+
+    return vec![Event::Read];
 }
 
-fn handle_read_second_request(conn: &mut Conn) {
+fn handle_read_second_request(conn: &mut Conn) -> Vec<Event> {
     let socket = &mut conn.socket;
-    println!("socket: {:?}", socket);
     read_until_would_block(socket, &mut conn.read_buf).unwrap();
 
     if conn.read_buf.len() < 4 {
-        return;
+        return vec![Event::Read];
     }
 
     // handle hdr
@@ -241,7 +213,7 @@ fn handle_read_second_request(conn: &mut Conn) {
     assert_eq!(atyp, 0x1);
 
     if conn.read_buf.len() < 10 {
-        return;
+        return vec![Event::Read];
     }
 
     let dst_addr = IpAddr::V4(Ipv4Addr::new(
@@ -252,20 +224,16 @@ fn handle_read_second_request(conn: &mut Conn) {
         &[conn.read_buf[8], conn.read_buf[9]][..]
     );
 
-    println!("second: {}:{}", dst_addr, dst_port);
-
     let s = TcpStream::connect(&SocketAddr::new(dst_addr, dst_port)).unwrap();
     conn.remote_socket = Some(s);
 
-
     conn.state = State::WriteSecondReply;
-    conn.next_event = Event::Write;
     conn.read_buf.clear();
 
-    // TODO handle errors
+    return vec![Event::Write];
 }
 
-fn handle_write_second_reply(conn: &mut Conn) {
+fn handle_write_second_reply(conn: &mut Conn) -> Vec<Event> {
     let socket = &mut conn.socket;
 
     if let Some(ref remote_socket) = conn.remote_socket {
@@ -281,7 +249,8 @@ fn handle_write_second_reply(conn: &mut Conn) {
             let size = socket.write(&reply).unwrap();
             assert_eq!(size, reply.len());
             conn.state = State::Proxy;
-            conn.next_event = Event::Register;
+
+            return vec![Event::Read, Event::RemoteRegister, Event::RemoteRead];
         } else {
             panic!("why");
         }
@@ -290,18 +259,48 @@ fn handle_write_second_reply(conn: &mut Conn) {
     }
 }
 
-fn handle_proxy(conn: &mut Conn, client_conn: bool, is_read: bool) {
-    // read <- client socket
-    // write -> remote socket
-    println!("yey handle_proxy: {}", client_conn);
-
-    // if is_read { .. }
-    if client_conn && is_read {
-        let socket = &mut conn.socket;
-        read_until_would_block(socket, &mut conn.read_buf).unwrap();
-
-        conn.state = State::Proxy;
-        conn.next_event = Event::RemoteWrite;
+fn handle_proxy(conn: &mut Conn, client_conn: bool, is_read: bool) -> Vec<Event> {
+    println!("handle_proxy: {} {}", client_conn, is_read);
+    match (client_conn, is_read) {
+        (true, true) => {
+            let socket = &mut conn.socket;
+            read_until_would_block(socket, &mut conn.read_buf).unwrap();
+            let mut ret = vec![Event::Read];
+            if !conn.already_remote_write {
+                ret.push(Event::RemoteWrite);
+                conn.already_remote_write = true;
+            }
+            ret
+        },
+        (true, false) => {
+            let socket = &mut conn.socket;
+            let size = socket.write(&conn.remote_buf).unwrap();
+            assert_eq!(size, conn.remote_buf.len());
+            conn.remote_buf.clear();
+            conn.already_write = false;
+            vec![Event::Read]
+        },
+        (false, false) => {
+            if let Some(ref mut socket) = conn.remote_socket {
+                let size = socket.write(&conn.read_buf).unwrap();
+                assert_eq!(size, conn.read_buf.len());
+                conn.read_buf.clear();
+            }
+            conn.already_remote_write = false;
+            vec![Event::RemoteRead]
+        },
+        (false, true) => {
+            // unwrap
+            if let Some(ref mut socket) = conn.remote_socket {
+                read_until_would_block(socket, &mut conn.remote_buf).unwrap();
+            }
+            let mut ret = vec![Event::RemoteRead];
+            if !conn.already_write {
+                ret.push(Event::Write);
+                conn.already_write = true;
+            }
+            ret
+        },
     }
 }
 
@@ -314,7 +313,6 @@ fn read_until_would_block(source: &mut Read, buf: &mut BytesMut) -> Result<usize
     loop {
         match source.read(&mut tmp_buf) {
             Ok(size) => {
-                println!("read size: {}", size);
                 buf.put(&tmp_buf[0 .. size]);
                 total_size += size;
             },
